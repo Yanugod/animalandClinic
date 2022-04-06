e9db2fcb397f8ae169aadf2455b9d862bc59268e
Yanuel Alcantara <llerlinalcantara@gmail.com>
2022-04-06 17:22:12 -0400

    APP WEB EN PUBLIC

---
+++ public/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--ESTILOS-->
+    <link rel="stylesheet" href="../public/Css/main.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <title>ANIMALAND</title>
+</head>
+
+<body>
+
+    <?php include('header.php');?>
+
+    <section id="about" class="hero__about">
+        <div class="hero__about-img">
+            <img src="img/doctor.jpg" class="hero__about-dr" alt="">
+            <a href="#nada">Read More</a>
+        </div>
+        <div class="hero__about-texts">
+            <div class="hero__about-title">
+                <h2>About</h2>
+            </div>
+            <div class="hero__about-paragraph">
+                <p>Animaland was created by former Minister of Public Health and Sanitation Dr. Rogers Claudelle Johnson
+                    in 1978, Hudson, New Jersey.</p>
+                <br>
+                <p>It arises from the need for the massive number of animals infected with bacteria at that time, there
+                    were few clinics that had a particular veterinarian, so the former minister decided to form a
+                    veterinarian in the center of Hudson, which was soon growing and expanding to what it is today.</p>
+                <br>
+                <p>Dr. Rogers Claudelle Johnson is always looking for ways to improve the quality of the service offered
+                    by the company, innovate and update what we need and is there to solve any precarious situation that
+                    an animal suffers. </p>
+            </div>
+        </div>
+    </section>
+
+    <section class="hero__newness">
+        <div class="hero__newness-cards">
+            <div class="newness-card">
+                <div class="hero__newmess-title">
+                    <h2>+200k<i class="bi bi-check-all"></i></h2>
+                </div>
+                <div class="hero__newness-paragraph">
+                    <p>Clients trust us.</p>
+                </div>
+            </div>
+        </div>
+        <div class="hero__newness-cards">
+            <div class="newness-card">
+                <div class="hero__newmess-title">
+                    <h2>+415k<i class="bi bi-currency-exchange"></i></h2>
+                </div>
+                <div class="hero__newness-paragraph">
+                    <p>Contribution raised by anonymous people and volunteers.</p>
+                </div>
+            </div>
+        </div>
+        <div class="hero__newness-cards newness-cards--red">
+            <div class="newness-card">
+                <div class="hero__newmess-title">
+                    <h2>+100k<i class="bi bi-balloon-heart"></i></h2>
+                </div>
+                <div class="hero__newness-paragraph">
+                    <p>Animals saved by us.</p>
+                </div>
+            </div>
+        </div>
+        <div class="hero__newness-cards">
+            <div class="newness-card">
+                <div class="hero__newmess-title">
+                    <h2>+55k<i class="bi bi-clipboard2-pulse"></i></h2>
+                </div>
+                <div class="hero__newness-paragraph">
+                    <p>Appointments we receive annually.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    </section>
+
+    
+
+
+    <script src="js/app.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
