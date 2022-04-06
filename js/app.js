2685d7399d70564fac18359b5de5a45900d5a253
Yanuel Alcantara <llerlinalcantara@gmail.com>
2022-04-06 17:27:21 -0400

    modificando cambios

--- js/app.js
+++
@@ -1,6 +0,0 @@
-const iconoMenu = document.querySelector('.menu-bar');
-const menu = document.querySelector('.hero__links');
-
-iconoMenu.addEventListener('click', ()=>{
-    menu.classList.toggle("menu-toggle");
-});
